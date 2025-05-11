0483c764b8303a36d9ef4eaa65ab200ec91db77b
Nandy <161332108+Fernandarabs@users.noreply.github.com>
2025-05-11 18:09:48 -0300

    Update mata.js

--- mata.js
+++ mata.js
@@ -9,11 +9,11 @@ const characters = [
 ];
 
 const characterImages = {
-   'Lyra': ['Lyra.png', 'Lyra.png'],
-   'Orion': ['Orion.png', 'Orion.png'],
-   'windy': ['windy.png', 'windy.png'],
-   'Sky': ['Sky.png', 'Sky.png'],
-   'Cloud': ['Cloud.png', 'Cloud.png'],
+   'Lyra': ['./imagens/Lyra.png', './imagens/Lyra.png'],
+   'Orion': ['./imagens/Orion.png', './imagens/Orion.png'],
+   'windy': ['./imagens/windy.png', './imagens/windy.png'],
+   'Sky': ['./imagens/Sky.png', './imagens/Sky.png'],
+   'Cloud': ['./imagens/Cloud.png', './imagens/Cloud.png'],
 };
 
 const characterDescriptions = {
@@ -81,7 +81,7 @@ const createCard = (character, image) => {
    const card = createElement('div', 'card');
    const front = createElement('div', 'face front');
    const back = createElement('div', 'face back');
-   front.style.backgroundImage = `url('${image}')`;
+   front.style.backgroundImage = `url('./imagens/${image}')`;
    card.appendChild(front);
    card.appendChild(back);
    card.addEventListener('click', revealCard);
