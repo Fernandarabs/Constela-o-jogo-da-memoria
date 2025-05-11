d3db3341683acf5b416e4a92998a0341885bebc1
Nandy <161332108+Fernandarabs@users.noreply.github.com>
2025-05-11 18:36:49 -0300

    Update mata.js

--- mata.js
+++ mata.js
@@ -81,7 +81,7 @@ const createCard = (character, image) => {
    const card = createElement('div', 'card');
    const front = createElement('div', 'face front');
    const back = createElement('div', 'face back');
-   front.style.backgroundImage = `url('../imagens/${image}')`;
+   front.style.backgroundImage = `url('${image}')`;
    card.appendChild(front);
    card.appendChild(back);
    card.addEventListener('click', revealCard);
